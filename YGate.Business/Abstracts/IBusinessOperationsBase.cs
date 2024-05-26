c17260c708b5a882646a0c31cea4ec45ddc988fa
Yusuf Kıdır <83632487+Yusefuyn-14@users.noreply.github.com>
2024-05-26 10:40:23 +0300

    Add files via upload

---
+++ YGate.Business/Abstracts/IBusinessOperationsBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YGate.Server.Entities.Abstracts;
+using YGate.Shared.Abstracts;
+
+namespace YGate.Business.Abstracts
+{
+    public interface IBusinessOperationsBase<T> : IGenericDataOperationsBase<T>
+        where T: class, IIndexable, new()
+    {
+    }
+}
